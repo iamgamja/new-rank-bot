6577f4b616d7edc04d996e5b34f2448b2fc925e1
iamgamja <hmmthinking@naver.com>
2022-10-04 13:00:57 +0900

    Create 날짜.ts

---
+++ src/modules/날짜.ts
@@ -0,0 +1,26 @@
+import { Module, applicationCommand } from '@pikokr/command.ts'
+import { Client } from '../structures/client'
+import { CommandInteraction } from 'discord.js'
+
+class 날짜 extends Module {
+  constructor(private cts: Client) {
+    super()
+  }
+
+  @applicationCommand({
+    command: {
+      type: 'CHAT_INPUT',
+      name: '날짜',
+      description: '2020년 10월 17일을 0일로 계산해서 오늘 날짜를 확인합니다.',
+    },
+  })
+  async 날짜(i: CommandInteraction) {
+    const diffms = new Date() - new Date('2022 10 17')
+    const diffday = Math.floor(  diffms / 1000 / 60 / 60 / 24  )
+    await i.reply(`오늘은 ${diffday}일 입니다.`)
+  }
+}
+
+export function install(cts: Client) {
+  return new 날짜(cts)
+}
