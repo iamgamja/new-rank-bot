588714fb86ccca0d8f407cadb287c1e18e16645f
iamgamja <hmmthinking@naver.com>
2022-10-06 10:15:28 +0900

    Update 프로필.ts

--- src/modules/프로필.ts
+++ src/modules/프로필.ts
@@ -45,7 +45,7 @@ class 프로필 extends Module {
           `${member.displayName}님의 정보:\n` +
           '```\n' +
           `${Data_Tears[userData.스탯.티어]} Lv. ${userData.스탯.레벨} / EXP ${userData.스탯.경험치} (다음 레벨까지 EXP ${남은경험치})\n` +
-          `공격력: ${userData.공격력}\n` +
+          `공격력: ${userData.공격력} / HP: ${userData.체력}\n` +
           `소지품:\n` +
           `  R ${userData.소지품.재화.R}\n` +
           '```',
