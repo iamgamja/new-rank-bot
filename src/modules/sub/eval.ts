1512ec3e7acfb9bacb0aba8f9a9f9c6d2d52ff0c
iamgamja <hmmthinking@naver.com>
2022-10-08 13:11:30 +0900

    eval: add editUserInfoMsg function

--- src/modules/sub/eval.ts
+++ src/modules/sub/eval.ts
@@ -3,6 +3,7 @@ import { Client } from '../../structures/client'
 import { CommandInteraction, GuildMember, TextChannel } from 'discord.js'
 import { DBData } from '../../../types/DBData'
 import calculateExp from '../../functions/calculateExp'
+import Data_Tears from '../../data/tears'
 
 class Eval extends Module {
   constructor(private cts: Client) {
@@ -28,6 +29,31 @@ class Eval extends Module {
     if ((i.member as GuildMember).id !== '526889025894875158') return
 
     const db = await (this.cts.client.channels.cache.get('1025653116441464842') as TextChannel).messages.fetch('1025653282254880829')
+    const data = JSON.parse(db.content)
+    const userInfoMsg = await (this.cts.client.channels.cache.get('1025347124294070282') as TextChannel).messages.fetch('1025975950439088168')
+
+    async function editUserInfoMsg() {
+      let r: string[] = []
+
+      for (let userID in data) {
+        const userData = data[userID]
+
+        r.push(
+          `<@${userID}>님의 정보:\n` +
+            '```\n' +
+            `${Data_Tears[userData.티어]} Lv. ${userData.레벨} / EXP ${userData.경험치}\n` +
+            `공격력: ${userData.공격력} / HP: ${userData.체력}\n` +
+            `소지품:\n` +
+            `  R ${userData.R}\n` +
+            `장착:\n` +
+            `  무기: ${userData.무기}` +
+            `  방어구: ${userData.방어구}` +
+            '```'
+        )
+      }
+
+      await userInfoMsg.edit(r.join('\n\n'))
+    }
 
     try {
       const result = eval(식)
