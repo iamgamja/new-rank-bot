6cc75b33b2eca63768bf512369cea5e1411211b7
iamgamja <hmmthinking@naver.com>
2022-10-10 15:26:44 +0900

    쿨타임 기능 추가

--- src/modules/dungeon.ts
+++ src/modules/dungeon.ts
@@ -5,6 +5,7 @@ import { DBData } from '../../types/DBData'
 import Data_Dungeon from '../data/Dungeon'
 import calculateExp from '../functions/calculateExp'
 import editUserInfoMsg from '../functions/editUserInfoMsg'
+import Data_CoolTime from '../data/cooltime'
 
 function makeCommandOption(name: string) {
   return {
@@ -22,60 +23,82 @@ async function makeCommandFunc(cts: Client, name: string) {
     const data = JSON.parse(db.content) as DBData
     const member = i.member as GuildMember
     const target = Data_Dungeon[name]
-    if (i.channelId !== target.channelID) {
-      await i.reply({ content: '```diff\n- ' + `<#${target.channelID}>` + '에서만 사용할 수 있습니다.\n```', ephemeral: true })
-      return
-    }
     if (member.user.id in data) {
+      if (i.channelId !== target.channelID) {
+        await i.reply({ content: '```diff\n- ' + `<#${target.channelID}>` + '에서만 사용할 수 있습니다.\n```', ephemeral: true })
+        return
+      }
+      const coolTimeDB = await (cts.client.channels.cache.get('1025653116441464842') as TextChannel).messages.fetch('1028912965786796144')
+      const coolTimeData = JSON.parse(coolTimeDB.content)
+      let ispossible_cooltime: boolean
+      if (member.user.id in coolTimeData[target.channelID]) {
+        if (coolTimeData[target.channelID][member.user.id] <= new Date().getTime()) {
+          ispossible_cooltime = true // 시간이 지났으므로 가능
+          coolTimeData[target.channelID][member.user.id] = new Date().getTime() + Data_CoolTime[target.channelID] * 1000
+        } else {
+          ispossible_cooltime = false
+        }
+      } else {
+        ispossible_cooltime = true // 아직 실행한 적이 없으면 가능
+        coolTimeData[target.channelID][member.user.id] = new Date().getTime() + Data_CoolTime[target.channelID] * 1000
+      }
+      if (!ispossible_cooltime) {
+        await i.reply({
+          content: '```diff\n- 쿨타임을 기다려주세요.\n' + `실행 가능한 시간은 <t:${Math.floor(coolTimeData[target.channelID][member.user.id] / 1000)}:R>입니다.` + '\n```',
+          ephemeral: true,
+        })
+        return
+      }
+
       const userData = data[member.user.id]
       if (target.공격력 !== 0 && Math.ceil(target.체력 / userData.공격력) <= Math.ceil(userData.체력 / target.공격력)) {
         await i.reply({ content: '```diff\n- 처치하지 못했습니다...\n```', ephemeral: true })
-      } else {
-        // 처치
-        userData.R += target.획득R
-
-        // 이전 누적 레벨 계산
-        let 이전tear = userData.티어
-        let 이전level = userData.레벨
-        while (이전tear) {
-          이전tear -= 1
-          이전level += (이전tear + 1) * 5
-        }
-        const 이전누적레벨 = 이전level
-
-        // 설정
-        const oldTear = userData.티어
-        const oldLevel = userData.레벨
-        const oldExp = userData.경험치
-        // this.logger.info(oldTear, oldLevel, oldExp + target[2])
-        const [newTear, newLevel, newExp] = calculateExp(oldTear, oldLevel, oldExp + target.획득경험치)
-        userData.티어 = newTear
-        userData.레벨 = newLevel
-        userData.경험치 = newExp
-
-        // 나중 누적 레벨 계산
-        let 나중tear = userData.티어
-        let 나중level = userData.레벨
-        while (나중tear) {
-          나중tear -= 1
-          나중level += (나중tear + 1) * 5
-        }
-        const 나중누적레벨 = 나중level
-
-        const 추가된누적레벨 = 나중누적레벨 - 이전누적레벨
-
-        // 공격력, 체력 수정
-        userData.공격력 += 추가된누적레벨
-        userData.체력 += 추가된누적레벨
-
-        await db.edit(JSON.stringify(data))
-
-        await editUserInfoMsg(cts, data)
-
-        await i.reply({ content: '```diff\n처치했습니다.\n' + `+ EXP ${target.획득경험치}\n+ R ${target.획득R}` + '```' })
-
-        /** @todo 획득 아이템 자동화 */
+        return
       }
+      // 처치
+      userData.R += target.획득R
+
+      // 이전 누적 레벨 계산
+      let 이전tear = userData.티어
+      let 이전level = userData.레벨
+      while (이전tear) {
+        이전tear -= 1
+        이전level += (이전tear + 1) * 5
+      }
+      const 이전누적레벨 = 이전level
+
+      // 설정
+      const oldTear = userData.티어
+      const oldLevel = userData.레벨
+      const oldExp = userData.경험치
+      // this.logger.info(oldTear, oldLevel, oldExp + target[2])
+      const [newTear, newLevel, newExp] = calculateExp(oldTear, oldLevel, oldExp + target.획득경험치)
+      userData.티어 = newTear
+      userData.레벨 = newLevel
+      userData.경험치 = newExp
+
+      // 나중 누적 레벨 계산
+      let 나중tear = userData.티어
+      let 나중level = userData.레벨
+      while (나중tear) {
+        나중tear -= 1
+        나중level += (나중tear + 1) * 5
+      }
+      const 나중누적레벨 = 나중level
+
+      const 추가된누적레벨 = 나중누적레벨 - 이전누적레벨
+
+      // 공격력, 체력 수정
+      userData.공격력 += 추가된누적레벨
+      userData.체력 += 추가된누적레벨
+
+      await db.edit(JSON.stringify(data))
+
+      await editUserInfoMsg(cts, data)
+
+      await i.reply({ content: '```diff\n처치했습니다.\n' + `+ EXP ${target.획득경험치}\n+ R ${target.획득R}` + '```' })
+
+      /** @todo 획득 아이템 자동화 */
     } else {
       await i.reply({ content: '```diff\n- 등록되지 않은 유저입니다.\n```', ephemeral: true })
     }
