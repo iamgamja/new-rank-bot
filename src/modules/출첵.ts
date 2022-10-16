3a90d766ba8795bde71d1171e2ccb5e20ad151f5
iamgamja <hmmthinking@naver.com>
2022-10-16 13:53:06 +0900

    patch

--- src/modules/출첵.ts
+++ src/modules/출첵.ts
@@ -1,11 +1,8 @@
-import { Module, applicationCommand, option } from '@pikokr/command.ts'
+import { Module, applicationCommand } from '@pikokr/command.ts'
 import { Client } from '../structures/client'
 import { CommandInteraction, GuildMember, TextChannel } from 'discord.js'
-import { DBData } from '../../types/DBData'
-import Data_Tears from '../data/tears'
-import calculateExp from '../functions/calculateExp'
-import addExp from '../functions/add/addExp'
-import addThing from '../functions/add/addThing'
+import { User } from '../class/User'
+import UserNotFoundError from '../class/error/UserNotFoundError'
 
 class 출첵 extends Module {
   constructor(private cts: Client) {
@@ -25,55 +22,58 @@ class 출첵 extends Module {
       return
     }
 
-    const member = i.member as GuildMember
+    try {
+      const member = i.member as GuildMember
+      const user = new User(this.cts, member)
+      await user._setup()
 
-    const 출첵coolTimeDB = await (this.cts.client.channels.cache.get('1025653116441464842') as TextChannel).messages.fetch('1030671119797207040')
-    const 출첵coolTimeData = JSON.parse(출첵coolTimeDB.content)
+      await i.deferReply()
 
-    let ispossible_cooltime: boolean
-    if (member.user.id in 출첵coolTimeData) {
+      const 출첵coolTimeDB = await (this.cts.client.channels.cache.get('1025653116441464842') as TextChannel).messages.fetch('1030671119797207040')
+      const 출첵coolTimeData = JSON.parse(출첵coolTimeDB.content)
+
+      let ispossible_cooltime: boolean
+      if (member.user.id in 출첵coolTimeData) {
+        const d = new Date()
+        d.setHours(d.getHours() + 9)
+        if (출첵coolTimeData[member.id] <= d.getTime()) {
+          ispossible_cooltime = true // 시간이 지났으므로 가능
+        } else {
+          ispossible_cooltime = false
+        }
+      } else {
+        ispossible_cooltime = true // 아직 실행한 적이 없으면 가능
+      }
+
+      if (!ispossible_cooltime) {
+        await i.editReply({
+          content: '```diff\n- 쿨타임을 기다려주세요.\n```\n' + `실행 가능한 시간: <t:${Math.floor((출첵coolTimeData[member.id] - 9 * 60 * 60 * 1000) / 1000)}:R>`,
+        })
+        return
+      }
+
       const d = new Date()
       d.setHours(d.getHours() + 9)
-      if (출첵coolTimeData[member.id] <= d.getTime()) {
-        ispossible_cooltime = true // 시간이 지났으므로 가능
-      } else {
-        ispossible_cooltime = false
-      }
-    } else {
-      ispossible_cooltime = true // 아직 실행한 적이 없으면 가능
-    }
 
-    if (!ispossible_cooltime) {
-      await i.reply({
-        content: '```diff\n- 쿨타임을 기다려주세요.\n```\n' + `실행 가능한 시간: <t:${Math.floor((출첵coolTimeData[member.id] - 9 * 60 * 60 * 1000) / 1000)}:R>`,
-        ephemeral: true,
-      })
-      return
-    }
+      d.setMilliseconds(0)
+      d.setSeconds(0)
+      d.setMinutes(0)
+      d.setHours(0)
+      d.setDate(d.getDate() + 1)
 
-    await i.deferReply()
+      출첵coolTimeData[member.id] = d.getTime()
+      await 출첵coolTimeDB.edit(JSON.stringify(출첵coolTimeData))
 
-    const d = new Date()
-    d.setHours(d.getHours() + 9)
+      await user.add('경험치', 50)
+      await user.add('R', 50)
 
-    d.setMilliseconds(0)
-    d.setSeconds(0)
-    d.setMinutes(0)
-    d.setHours(0)
-    d.setDate(d.getDate() + 1)
-
-    출첵coolTimeData[member.id] = d.getTime()
-    await 출첵coolTimeDB.edit(JSON.stringify(출첵coolTimeData))
-
-    const result = await addExp(this.cts, member, 50)
-    const result2 = await addThing(this.cts, member, 'R', 50)
-
-    if (result && result2) {
       await i.editReply({
         content: '```diff\n출첵했습니다.\n+ EXP 50\n+ R 50\n```',
       })
-    } else {
-      await i.editReply({ content: '```diff\n- 등록되지 않은 유저입니다.\n```' })
+    } catch (e) {
+      if (e instanceof UserNotFoundError) {
+        await i.editReply({ content: '```diff\n- 등록되지 않은 유저입니다.\n```' })
+      }
     }
   }
 }
