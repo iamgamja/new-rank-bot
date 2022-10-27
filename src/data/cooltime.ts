2b26a137af1029dbc1332ebcdfd7962c218a431a
iamgamja <hmmthinking@naver.com>
2022-10-27 18:52:27 +0900

    Update cooltime.ts

--- src/data/cooltime.ts
+++ src/data/cooltime.ts
@@ -8,6 +8,7 @@ const Data_CoolTime: { [channelId: Snowflake]: number } = {
   '1026161001407725588': 30,
   '1026124882746032249': 1 * 60 * 60,
   '1026129415102550036': 6 * 60 * 60,
+  '1032216362103353384': 30,
 }
 
 export default Data_CoolTime
