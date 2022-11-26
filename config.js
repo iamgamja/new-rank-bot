7399a5e83173ee8a6c54d720e9f09e6b39549ae4
iamgamja <iamgamja@users.noreply.github.com>
2022-11-26 08:01:57 +0000

    deploy: 58c2ac55e1b007c2b381965d5f87bfd5dcea4c9f

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-const TOKEN = process.env.TOKEN || require('../config.json').token;
+const TOKEN = process.env.TOKEN || require('./config.json').token;
 exports.config = {
     token: TOKEN,
     slash: {
