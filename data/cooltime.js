5f81ba7e8d25c0d335205365cdb9dc0f361121f0
iamgamja <iamgamja@users.noreply.github.com>
2023-01-08 06:24:51 +0000

    deploy: 5c37fb52586aff52b2d75b0ff2ae705681b169ae

--- data/cooltime.js
+++ data/cooltime.js
@@ -9,6 +9,7 @@ const Data_CoolTime = {
     '1026124882746032249': 1 * 60 * 60,
     '1026129415102550036': 6 * 60 * 60,
     '1032216362103353384': 30,
+    '1035476140753703012': 30,
 };
 exports.default = Data_CoolTime;
 //# sourceMappingURL=cooltime.js.map
